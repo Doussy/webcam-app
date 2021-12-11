455b35458cb2de44b735ac26c3e58a5e5d902f9a
Andre Doussy <andre@angrybytes.com>
2021-12-11 12:08:58 +0100

    Create project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Webcam App
+
+## Build Setup
+
+```bash
+# Install dependencies
+$ yarn
+
+# Serve with hot reload at localhost:3000
+$ yarn dev
+
+# Build for production and launch server
+$ yarn build
+$ yarn start
+
+# TypeScript check
+$ yarn type-check
+```
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout title="Home | Webcam App">
+    <div className="m-4 text-center">
+      <div className="text-2xl">Webcam App</div>
+    </div>
+
+  </Layout>
+);
+
+export default IndexPage;
